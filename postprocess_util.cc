61fa5ef63205a23a8601ed9c05abc59e3792e913
Ronald Claveau <sousmangoosta@aliel.fr>
2024-05-17 17:13:15 +0200

    Add NMS and replace model with the best trained one

--- postprocess_util.cc
+++ postprocess_util.cc
@@ -1,10 +1,50 @@
 #include <cmath>
 #include "nn_sdk.h"
 #include "postprocess_util.h"
+#include <vector>
 
-float logistic_activate(float x)
+float iou(const dBox boxA, const dBox boxB)
 {
-    return (float)1./(float)(1. + std::exp(-x));
+    const float eps = 1e-6;
+    float iou = 0.f;
+    float areaA = (boxA.xmax - boxA.xmin) * (boxA.ymax - boxA.ymin);
+    float areaB = (boxB.xmax - boxB.xmin) * (boxB.ymax - boxB.ymin);
+    float x1 = std::max(boxA.xmin, boxB.xmin);
+    float y1 = std::max(boxA.ymin, boxB.ymin);
+    float x2 = std::min(boxA.xmax, boxB.xmax);
+    float y2 = std::min(boxA.ymax, boxB.ymax);
+    float w = std::max(0.f, x2 - x1);
+    float h = std::max(0.f, y2 - y1);
+    float inter = w * h;
+    iou = inter / (areaA + areaB - inter + eps);
+    return iou;
+}
+
+int compare (const void* pa, const void* pb) {
+    dBox a = *(dBox*) pa;
+    dBox b = *(dBox*) pb;
+    return a.score + a.objectClass < b.score + b.objectClass;
+}
+
+void nms(dBox* tBox, float iou_threshold, int rows) {
+    qsort(tBox, rows, sizeof(dBox), compare);
+    for(int i = 0; i < rows; i++) {
+        if (tBox[i].score == 0.f)
+        {
+            continue;
+        }
+        for (int j = i + 1; j < rows; ++j)
+        {
+            if (tBox[i].objectClass != tBox[j].objectClass)
+            {
+                break;
+            }
+            if (iou(tBox[i], tBox[j]) > iou_threshold)
+            {
+                tBox[j].score = 0.f;
+            }
+        }
+    }
 }
 
 void postprocess_ssd(nn_output *pout, unsigned int *detNum, dBox *pBox)
@@ -13,6 +53,7 @@ void postprocess_ssd(nn_output *pout, unsigned int *detNum, dBox *pBox)
     ssd_buffer[0] = (float*)pout->out[0].buf;
     ssd_buffer[1] = (float*)pout->out[1].buf;
     ssd_buffer[2] = (float*)pout->out[2].buf;
+    dBox tBox[MAX_DETECT_NUM];
 
     struct Anchor {
         double y;
@@ -55,19 +96,26 @@ void postprocess_ssd(nn_output *pout, unsigned int *detNum, dBox *pBox)
         box.ymax = ycenter + half_h;
         box.xmax = xcenter + half_w;
         for (int j = 0; j < 91; j++) {
-            float prob = logistic_activate((float) ssd_buffer[0][i*91+j]);
-            if (prob > 0.600000023842) {
-                //printf("%d : %f %f %f %f\n", i, box.ymin, box.xmin, box.ymax, box.xmax);
-                pBox[num_detections].ymin = box.ymin;
-                pBox[num_detections].xmin = box.xmin;
-                pBox[num_detections].ymax = box.ymax;
-                pBox[num_detections].xmax = box.xmax;
-                pBox[num_detections].score = prob;
-                pBox[num_detections].objectClass = (float)j-1;
+            float prob = ssd_buffer[0][i*91+j];
+            if (prob > 0.6) {
+                tBox[num_detections].ymin = box.ymin;
+                tBox[num_detections].xmin = box.xmin;
+                tBox[num_detections].ymax = box.ymax;
+                tBox[num_detections].xmax = box.xmax;
+                tBox[num_detections].score = prob;
+                tBox[num_detections].objectClass = (float)j-1;
                 num_detections++;
             }
         }
     }
-    *detNum = num_detections;
-    //printf("found %d elements\n", num_detections);
+
+    nms(tBox, 0.6000000238418579, num_detections);
+    int nms_detections = 0;
+    for(int i = 0; i < num_detections; i++) {
+        if(tBox[i].score > 0.f) {
+            pBox[nms_detections] = tBox[i];
+            nms_detections++;
+        }
+    }
+    *detNum = nms_detections;
 }
